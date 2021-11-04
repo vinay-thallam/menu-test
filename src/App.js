b934988aeee5caa6b57ea3a60d56ac3f9f943d91
Vinay Thallam <vinay.thallam@jifflenow.com>
2021-11-04 13:56:28 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import 'react-app-polyfill/ie11'
 import 'react-app-polyfill/stable'
 import React from 'react'
-import { Client as Styletron } from 'styletron-engine-atomic'
-import { Provider as StyletronProvider } from 'styletron-react'
+// import { Client as Styletron } from 'styletron-engine-atomic'
+// import { Provider as StyletronProvider } from 'styletron-react'
 import { LightTheme, BaseProvider, styled } from 'baseui'
 // import { StatefulMenu } from 'baseui/menu'
 import { Button } from 'baseui/button'
 import './App.css'
 
-const engine = new Styletron()
+// const engine = new Styletron()
 const Centered = styled('div', {
   display: 'flex',
   justifyContent: 'center',
@@ -23,11 +23,11 @@ const Centered = styled('div', {
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function App() {
   return (
-    <StyletronProvider value={engine}>
-      <BaseProvider theme={LightTheme}>
-        <Centered>
-          <Button>Test Button</Button>
-          {/* <StatefulMenu
+    // <StyletronProvider value={engine}>
+    <BaseProvider theme={LightTheme}>
+      <Centered>
+        <Button>Test Button</Button>
+        {/* <StatefulMenu
             items={[
               { label: 'Item One' },
               { label: 'Item Two' },
@@ -36,9 +36,9 @@ function App() {
             ]}
             onItemSelect={itemClick}
           /> */}
-        </Centered>
-      </BaseProvider>
-    </StyletronProvider>
+      </Centered>
+    </BaseProvider>
+    // </StyletronProvider>
   )
 }
 
