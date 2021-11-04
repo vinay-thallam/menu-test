74c20889b3a490b3b5ac6eca23e17c9296ce816f
Vinay Thallam <vinay.thallam@jifflenow.com>
2021-11-04 14:00:27 +0530

    test

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import 'react-app-polyfill/stable'
 import React from 'react'
 // import { Client as Styletron } from 'styletron-engine-atomic'
 // import { Provider as StyletronProvider } from 'styletron-react'
-import { LightTheme, BaseProvider, styled } from 'baseui'
+// import { LightTheme, BaseProvider, styled } from 'baseui'
 // import { StatefulMenu } from 'baseui/menu'
-import { Button } from 'baseui/button'
+// import { Button } from 'baseui/button'
 import './App.css'
 
 // const engine = new Styletron()
-const Centered = styled('div', {
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  height: '100%',
-})
+// const Centered = styled('div', {
+//   display: 'flex',
+//   justifyContent: 'center',
+//   alignItems: 'center',
+//   height: '100%',
+// })
 
 // function itemClick(item) {
 //   console.log('item :', item.item)
@@ -23,21 +23,22 @@ const Centered = styled('div', {
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function App() {
   return (
+    <div>Hello world!!</div>
     // <StyletronProvider value={engine}>
-    <BaseProvider theme={LightTheme}>
-      <Centered>
-        <Button>Test Button</Button>
-        {/* <StatefulMenu
-            items={[
-              { label: 'Item One' },
-              { label: 'Item Two' },
-              { label: 'Item Three' },
-              { label: 'Item Four' },
-            ]}
-            onItemSelect={itemClick}
-          /> */}
-      </Centered>
-    </BaseProvider>
+    // <BaseProvider theme={LightTheme}>
+    //   <Centered>
+    //     <Button>Test Button</Button>
+    //     {/* <StatefulMenu
+    //         items={[
+    //           { label: 'Item One' },
+    //           { label: 'Item Two' },
+    //           { label: 'Item Three' },
+    //           { label: 'Item Four' },
+    //         ]}
+    //         onItemSelect={itemClick}
+    //       /> */}
+    //   </Centered>
+    // </BaseProvider>
     // </StyletronProvider>
   )
 }
